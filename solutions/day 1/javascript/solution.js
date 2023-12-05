c8f2760e05e4492ea9ff8f6d77e55897aa66091e
josssuu <joosep.postov@gmail.com>
2023-12-05 21:45:21 +0200

    day 1 -  javascript solution

---
+++ solutions/day 1/javascript/solution.js
@@ -0,0 +1,25 @@
+function findLastNumberChar(str) {
+    var reverseIdx = str.split("").reverse().findIndex((char) => !isNaN(parseInt(char)));
+    return str[str.length - reverseIdx - 1];
+};
+
+function findFirstNumberChar(str) {
+    var idx = str.split("").findIndex((char) => !isNaN(parseInt(char)));
+    return str[idx];
+};
+
+function extractCalibrationValue(row) {
+    return parseInt(findFirstNumberChar(row) + findLastNumberChar(row));
+};
+
+var calibrationValues = [];
+
+const fs = require("fs");
+fs.readFileSync("../input.txt")
+    .toString()
+    .split("\n")
+    .forEach(row => calibrationValues.push(extractCalibrationValue(row)));
+
+let sum = calibrationValues.reduce((sum, calibrationValue) => sum += calibrationValue);
+
+console.log("sum of all of the calibration values:", sum);
