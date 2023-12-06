18c9713c2b1b439f400827a7d7fd2a6db256d2b5
josssuu <joosep.postov@gmail.com>
2023-12-06 16:56:06 +0200

    feat: Solve day 1 part 2

--- solutions/day 1/javascript/solution.js
+++ solutions/day 1/javascript/solution.js
@@ -1,16 +1,37 @@
-function findLastNumberChar(str) {
-    var reverseIdx = str.split("").reverse().findIndex((char) => !isNaN(parseInt(char)));
-    return str[str.length - reverseIdx - 1];
-};
 
-function findFirstNumberChar(str) {
-    var idx = str.split("").findIndex((char) => !isNaN(parseInt(char)));
-    return str[idx];
-};
+const reverseString = (str) => result = str?.split('').reverse().join('');
 
-function extractCalibrationValue(row) {
-    return parseInt(findFirstNumberChar(row) + findLastNumberChar(row));
-};
+function convertStringToNumber(str) {
+    let numberRegex = /^[0-9]$/;
+
+    if (numberRegex.test(str)) return parseInt(str);
+
+    let names = {
+        "zero"  : 0,
+        "one"   : 1,
+        "two"   : 2,
+        "three" : 3,
+        "four"  : 4,
+        "five"  : 5,
+        "six"   : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine"  : 9,
+    };
+
+    return names[str];
+}
+
+function getCalibrationValue(str) {
+    let numbersRegex = "one|two|three|four|five|six|seven|eight|nine";
+    let pattern = "[0-9]|" + numbersRegex;
+    let reversePattern = "[0-9]|" + reverseString(numbersRegex);
+
+    let firstNumber = convertStringToNumber(str.match(pattern)?.[0]);
+    let lastNumber = convertStringToNumber(reverseString(reverseString(str).match(reversePattern)?.[0]));
+
+    return parseInt(firstNumber?.toString() + lastNumber?.toString()) || 0;
+}
 
 var calibrationValues = [];
 
@@ -18,7 +39,7 @@ const fs = require("fs");
 fs.readFileSync("../input.txt")
     .toString()
     .split("\n")
-    .forEach(row => calibrationValues.push(extractCalibrationValue(row)));
+    .forEach(row => calibrationValues.push(getCalibrationValue(row)));
 
 let sum = calibrationValues.reduce((sum, calibrationValue) => sum += calibrationValue);
 
